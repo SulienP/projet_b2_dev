8b7a2f78a45ff99d1a398e819f12d2a1e5eb6fa1
Harcadakad <evan.guillet@ynov.com>
2024-04-22 14:55:26 +0200

    :sparkles: Registration system up and running + add login and logout system

--- yculture/yculture/urls.py
+++ yculture/yculture/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from accounts.views import signup
+from home.views import index
+from accounts.views import signup, logout_user, login_user
 
 from yculture import settings
 
 urlpatterns = [
+    path('', index, name="index"),
     path('admin/', admin.site.urls),
     path('signup/', signup, name="signup"),
+    path('login/', login_user, name="login"),
+    path('logout/' , logout_user, name="logout")
 ]
