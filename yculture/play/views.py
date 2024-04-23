5d6e452a031b5658b791ac305798aa72d4a5fe78
SulienP <112190974+SulienP@users.noreply.github.com>
2024-04-24 00:40:50 +0200

    :sparkles: | start game

--- yculture/play/views.py
+++ yculture/play/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import redirect, render
 
 def play(request):
     value = gamemanager(request)
-    print (value)
     if (value == "play"):
         
         return render(request, 'play/play.html', {'value': value})
