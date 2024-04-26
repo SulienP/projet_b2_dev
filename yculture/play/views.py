43b4e0233c56039ca7228e19c9d756a76d1389bd
Harcadakad <evan.guillet@ynov.com>
2024-04-26 14:56:21 +0200

    :lipstick: Improve play page interface

--- yculture/play/views.py
+++ yculture/play/views.py
@@ -2,9 +2,13 @@ from play.gamemanager import gamemanager
 from django.shortcuts import redirect, render
 
 def play(request):
+    return render(request, 'play/play.html')
+    '''
     value = gamemanager(request)
     if (value == "play"):
         
         return render(request, 'play/play.html', {'value': value})
     else:
         return redirect('index')
+        '''
+    
